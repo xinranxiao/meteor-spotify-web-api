8debbc684a4d4f4ddb1202077472278d9ce537f8
Xinran Xiao <xinranxiao@gmail.com>
2016-02-03 13:50:36 -0800

    Update config.js

--- examples/1/server/config.js
+++ examples/1/server/config.js
@@ -2,8 +2,8 @@ ServiceConfiguration.configurations.update(
     { "service": "spotify" },
     {
       $set: {
-        "clientId": "6008c94f99884ff49576f4e763f19d58",
-        "secret": "42ffdf7297b148e79588274548fac0be"
+        "clientId": "<your client id>",
+        "secret": "<your secret key>"
       }
     },
     { upsert: true }
